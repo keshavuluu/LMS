3c785addcecbde72fea3b3597d767facd3315965
keshavuluu <keshavulumuppa@example.com>
2025-04-14 13:04:02 +0530

    first

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -4,8 +4,7 @@ import User from "../models/User.js";
 const clearWebhooks = async (req, res) => {
   try {
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-    
-    // Verify webhook signature
+
     await whook.verify(JSON.stringify(req.body), {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from "./configs/mongodb.js";
-import {clearWebhooks} from "./controllers/webhooks.js";
+import clearWebhooks from "./controllers/webhooks.js";
 const app = express();
 
 await connectDB();
